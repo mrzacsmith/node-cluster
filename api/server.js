b8f8074be71e11e3a618b31760c49573648a429b
Zac Smith <zrs3141592@gmail.com>
2021-03-11 21:52:40 -0700

    add rate limit

--- api/server.js
+++ api/server.js
@@ -7,9 +7,21 @@ require('colors')
 
 const server = express()
 
+// db connection
+
+// middleware
 server.use(express.json())
 server.use(helmet())
 server.use(morgan('dev'))
 server.use(cors())
+
+// rate limit
+const limiter = new RateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  delay: 0,
+})
+
+// routes
 
 module.exports = server
